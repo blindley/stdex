b0f6aa26fd79f44408a016c6ecde05d920103585
Benjamin Lindley <benjameslindley@gmail.com>
2018-10-29 00:43:18 -0500

    ignore examples/scratch.rs

--- examples/scratch.rs
+++ examples/scratch.rs
@@ -2,17 +2,31 @@
 extern crate stdex;
 use stdex::{
     random::MT19937_32,
-    collections::binary_heap::BinaryHeap
+    collections::BinaryHeap
 };
 
 fn main() {
-    let mut heap = BinaryHeap::min_heap();
+    let mut heap = BinaryHeap::max_heap();
     let mut rng = MT19937_32::new();
-    for _ in 0..20 {
+    let mut values_generated = 0;
+
+    for _ in 0..15 {
         heap.push(rng.generate());
+        values_generated += 1;
+    }
+
+    while *heap.peek().unwrap() >= 1000000000 {
+        let value = rng.generate();
+        values_generated += 1;
+        if value < *heap.peek().unwrap() {
+            heap.pop();
+            heap.push(value);
+        }
     }
 
     while let Some(value) = heap.pop() {
         println!("{}", value);
     }
+
+    println!("{} values generated", values_generated);
 }

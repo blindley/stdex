fa6eaa30a78fdd75a27df76d1b6be866abc84fbe
Benjamin Lindley <benjameslindley@gmail.com>
2018-10-29 00:28:51 -0500

    brought BinaryHeap in to collections namespace

--- examples/scratch.rs
+++ examples/scratch.rs
@@ -1,23 +1,18 @@
 #![allow(unused_imports, dead_code)]
 extern crate stdex;
+use stdex::{
+    random::MT19937_32,
+    collections::binary_heap::BinaryHeap
+};
 
 fn main() {
-    use stdex::random::{MT19937_32, MT19937_64};
-
-    let mut rng: MT19937_64 = unsafe { std::mem::zeroed() };
-    unsafe {
-        let ptr = &mut rng as *mut _ as *mut u8;
-        let ptr = ptr.offset(100);
-        *ptr = 1;
+    let mut heap = BinaryHeap::min_heap();
+    let mut rng = MT19937_32::new();
+    for _ in 0..20 {
+        heap.push(rng.generate());
     }
-    let mut non_zero_count = 0;
-    for i in 0..6000 {
-        if non_zero_count >= 1000 { break; }
 
-        let value = rng.generate();
-        if value != 0 {
-            println!("{}: {}", i, value);
-            non_zero_count += 1;
-        }
+    while let Some(value) = heap.pop() {
+        println!("{}", value);
     }
 }
--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,1 +1,2 @@
 pub mod binary_heap;
+pub use self::binary_heap::BinaryHeap;
